4c231543ed6157541d94f1509647cda3ebdc157d
HamedKamal <hamed6kamal@gmail.com>
2022-11-13 14:31:53 -0800

    all posible values

--- watermelom.py
+++ watermelom.py
@@ -1,5 +1,12 @@
 x = int(input('Enter The Kilos '))
+def possible_values(y):
+    a=0
+    while(a<=y):
+        a+=2
+        y-=2
+        print(str(a)+'  and  '+str(y))
 if x%2==0 and x!=2:
+    possible_values(x)
     print('yes')
     
 else:
