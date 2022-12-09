1aba6a93f62ad9845290987d5252ea96e5045933
HamedKamal <hamed6kamal@gmail.com>
2022-12-09 23:36:12 +0200

    done without functions

--- complex_calculator.py
+++ complex_calculator.py
@@ -3,22 +3,50 @@ user_input = input("enter your exppresion : ")
 splitted_text_list = user_input.split()
 
 while len(splitted_text_list) != 1:
+    
     for index, item in enumerate(splitted_text_list):
         # print(index,' ',item)
-        
-        if item == '*' and index==1:
-                value = int(splitted_text_list[index-1]) * int(splitted_text_list[index+1])
+        if ('/' in splitted_text_list or '*' in splitted_text_list):
+            if item == '*':
+                value = float(
+                    splitted_text_list[index-1]) * float(splitted_text_list[index+1])
                 print(value)
                 del splitted_text_list[index+1]
                 del splitted_text_list[index]
                 del splitted_text_list[index-1]
                 splitted_text_list.insert(index-1, value)
                 print(splitted_text_list)
-        elif item == '/'and index==1:
-                value = int(splitted_text_list[index-1]) / int(splitted_text_list[index+1])
+                break
+            elif item == '/':
+                value = float(
+                    splitted_text_list[index-1]) / float(splitted_text_list[index+1])
                 print(value)
                 del splitted_text_list[index+1]
                 del splitted_text_list[index]
                 del splitted_text_list[index-1]
                 splitted_text_list.insert(index-1, value)
                 print(splitted_text_list)
+                break
+
+        else:
+
+            if item == '+':
+                value = float(
+                    splitted_text_list[index-1]) + float(splitted_text_list[index+1])
+                print(value)
+                del splitted_text_list[index+1]
+                del splitted_text_list[index]
+                del splitted_text_list[index-1]
+                splitted_text_list.insert(index-1, value)
+                print(splitted_text_list)
+                break
+            elif item == '-':
+                value = float(
+                    splitted_text_list[index-1]) - float(splitted_text_list[index+1])
+                print(value)
+                del splitted_text_list[index+1]
+                del splitted_text_list[index]
+                del splitted_text_list[index-1]
+                splitted_text_list.insert(index-1, value)
+                print(splitted_text_list)
+                break
