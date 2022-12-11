2c310503d7469d86ac578b1de240aae8f168c876
HamedKamal <hamed6kamal@gmail.com>
2022-12-12 00:03:46 +0200

    Master

--- complex_calculator.py
+++ complex_calculator.py
@@ -1,42 +1,47 @@
+print('enter your expression like this 5 * 9 - 8 / 7')
 user_input = input("Calculate : ")
 
 splitted_text_list = user_input.split()
 
 
-# recommend uncomment print functions 
+# recommend uncomment print functions
 def times(index):
     value = float(
         splitted_text_list[index-1]) * float(splitted_text_list[index+1])
-    print(value)
+    # print(value)
     del splitted_text_list[index+1]
     del splitted_text_list[index]
     del splitted_text_list[index-1]
     splitted_text_list.insert(index-1, value)
     # print(splitted_text_list)
+
+
 def devide(index):
     value = float(
         splitted_text_list[index-1]) / float(splitted_text_list[index+1])
-    print(value)
+    # print(value)
     del splitted_text_list[index+1]
     del splitted_text_list[index]
     del splitted_text_list[index-1]
     splitted_text_list.insert(index-1, value)
     # print(splitted_text_list)
+
 
 def plus(index):
     value = float(
         splitted_text_list[index-1]) + float(splitted_text_list[index+1])
-    print(value)
+    # print(value)
     del splitted_text_list[index+1]
     del splitted_text_list[index]
     del splitted_text_list[index-1]
     splitted_text_list.insert(index-1, value)
     # print(splitted_text_list)
 
+
 def minus(index):
     value = float(
         splitted_text_list[index-1]) - float(splitted_text_list[index+1])
-    print(value)
+    # print(value)
     del splitted_text_list[index+1]
     del splitted_text_list[index]
     del splitted_text_list[index-1]
@@ -62,5 +67,5 @@ while len(splitted_text_list) != 1:
             elif item == '-':
                 minus(index)
                 break
-print('Your answer is ',splitted_text_list)
+print('Your answer is ', splitted_text_list)
 # after 2 hours i did it 🎉
---
+++ hazem.py
@@ -0,0 +1,7 @@
+user_name=input('enter your name : ')
+print ('hello',user_name)
+user_age=int(input('enter your age : '))
+if user_age >18:
+    print ('you are old ')
+else:
+    print('you are young ')
