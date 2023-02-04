f0d7a7bfaa147ba4a0c0d2920b559d4585201cb1
HamedKamal <hamed6kamal@gmail.com>
2023-02-05 00:53:08 +0200

    CIRCLE

--- geometric_shapes.py
+++ geometric_shapes.py
@@ -25,7 +25,7 @@ class rectangle(shape):
 
     def premiter(self):
         print(f"the premiter is {2*(self.lenth+self.width)}")
-
+        print(type(self.lenth))
 
 class NonRegTriangle(shape):
     def area(self):
@@ -36,69 +36,98 @@ class NonRegTriangle(shape):
 class Regular(shape):
     def premiter(self):
         print(f"the premiter is {self.getNofSides()*self.lenth}")
-
     def area(self):
         print(
             f"the area is {(self.getNofSides()*self.lenth**2)/(4*(math.tan((180/self.getNofSides())*math.pi/180)))}")
 
-class Circle:
-    def __init__(self,Xcenter,Ycenter,raduis) :
-        self.x=Xcenter,
-        self.y=Ycenter,
-        self.r=raduis,
-        
+class Circle(shape):
+    def generalForm(self):
+        newX=-2*self.lenth
+        newY=-2*self.width
+        r=self.getNofSides()
+        newR=(self.lenth**2)+(self.width**2)-(r**2)
+        #  if all is positive
+        condition=(self.lenth**2)+(self.width**2)-newR
+        if condition>0:
+            print(f"general equation : x² + y² + {newX}x + {newY}y + {newR} = 0")
+        else:
+            print('general equation eroeoeoerrr')
     def standardForm(self):
-        newX=-1*self.x
-        newY=-1*self.y
+        newX=-1*self.lenth
+        newY=-1*self.width
         
+            
+        r=self.getNofSides()
+        
+        newY=float(newY)
+        newX=float(newX)
+        r=float(r)
         # if x and y are positve
         if newX > 0 and newY > 0:
-            return f"(x + {newX})²+(y + {newY})²= {self.r **2}"
+            print(f"standard equation = (x + {newX})²+(y + {newY})²= {r**2}")
         # if x negative and y positve
         elif newX <0 and newY >0:
-            return f"(x  {newX})²+(y + {newY})²= {self.r **2}"
+            print(f"standard equation = (x  {newX})²+(y + {newY})²= {r**2}")
         # if x positive and y negative
         elif newX >0 and newY <0:
-            return f"(x+ {newX})²+(y  {newY})²= {self.r **2}"
+            print(f"standard equation = (x+ {newX})²+(y {newY})²= {r**2}")
         # if x  and y are  negative
-        elif newX >0 and newY <0:
-            return f"(x {newX})²+(y  {newY})²= {self.r **2}"
+        elif newX <0 and newY <0:
+            print(f"standard equation = (x {newX})²+(y {newY})²= {r**2}")
+    def premiter(self):
+        r=self.getNofSides()
+        
+        print(f"the Circle Circmfrance = {2*math.pi*r}")
+    def area(self):
+        r=self.getNofSides()
+        
+        print(f"the Circle Area = {math.pi*(r**2)}")
+    
 
 
-xCenter=float(input('enter the x of centr '))
-yCenter=float(input('enter the y of centr '))
-raduis=float(input('enter the y of centr '))
-mycircle=Circle(Xcenter=xCenter,Ycenter=yCenter,raduis=raduis)
-print(mycircle.standardForm())
-# polygonOrNot = input('Is it a regular polygon ? (y/n)  : ')
-# if polygonOrNot == 'y':
-#     user_side_number = int(input('enter the number of sides  : '))
-#     user_side_lenth = int(input('enter the lenth of side  : '))
-#     mypolygon = Regular(
-#         lenth=user_side_lenth,
-#         width=user_side_lenth)
-#     mypolygon.setNofSides(user_side_number)
-#     mypolygon.premiter()
-#     mypolygon.area()
-# elif polygonOrNot == 'n':
-#     user_side_number = int(input('enter the number of sides  : '))
-#     if user_side_number == 3:
-#         user_lenth = int(input('enter the base lenth  : '))
-#         user_hight = int(input('enter the hight lenth  : '))
+polygonOrNot = input('Is it a regular polygon ? (y/n)  : ')
+if polygonOrNot == 'y':
+    isitcircle=input('Is it a regular circle ? (y/n)  :')
+    if isitcircle=="y":
+        xCenter=float(input('enter the x of center '))
+        yCenter=float(input('enter the y of center '))
+        raduis=float(input('enter the Radius of circle '))
+        mycircle=Circle(lenth=xCenter,width=yCenter,)
+        mycircle.setNofSides(raduis)
+        mycircle.standardForm()
+        mycircle.generalForm()
+        mycircle.premiter()
+        mycircle.area()
+    elif isitcircle == "n":
+        user_side_number = int(input('enter the number of sides  : '))
+        user_side_lenth = int(input('enter the lenth of side  : '))
+        mypolygon = Regular(
+            lenth=user_side_lenth,
+            width=user_side_lenth)
+        mypolygon.setNofSides(user_side_number)
+        mypolygon.premiter()
+        mypolygon.area()
+    else:
+        print('erererrrrrrrrooeroeroero')
+elif polygonOrNot == 'n':
+    user_side_number = int(input('enter the number of sides  : '))
+    if user_side_number == 3:
+        user_lenth = int(input('enter the base lenth  : '))
+        user_hight = int(input('enter the hight lenth  : '))
 
-#         user_triangle = NonRegTriangle(
-#             lenth=user_lenth, width=user_hight)
-#         user_triangle.area()
+        user_triangle = NonRegTriangle(
+            lenth=user_lenth, width=user_hight)
+        user_triangle.area()
 
-#     elif user_side_number == 4:
+    elif user_side_number == 4:
 
-#         user_side_lenth = int(input('enter the lenth of side  : '))
-#         user_side_width = int(input('enter the lenth of width  : '))
-#         myrectangle = rectangle(
-#             lenth=user_side_lenth, width=user_side_width)
-#         myrectangle.premiter()
-#         myrectangle.area()
-#     else:
-#         print('er')
-# else:
-#     print('erorrrrrrr')
+        user_side_lenth = int(input('enter the lenth of side  : '))
+        user_side_width = int(input('enter the lenth of width  : '))
+        myrectangle = rectangle(
+            lenth=user_side_lenth, width=user_side_width)
+        myrectangle.premiter()
+        myrectangle.area()
+    else:
+        print('er')
+else:
+    print('erorrrrrrr')
